a152eeb95d39f5a2feebd6271a12a26a9f4fef92
David Kuzheev <dmonares@bk.ru>
2024-11-01 15:07:48 +0300

    First Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [
+    "@vueuse/nuxt",
+    "@nuxt/image",
+    "@pinia/nuxt",
+    "@nuxt/fonts",
+    "dayjs-nuxt",
+    "nuxt-typed-router",
+    "nuxt-file-storage",
+  ],
+  fonts: {
+    families: [{ name: "montserrat", provider: "google" }],
+  },
+  css: ["./assets/scss/style.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "~/assets/scss/base/normalize" as *;
+            @use "~/assets/scss/base/mixins" as *;
+            @use "~/assets/scss/base/globals" as *;
+            @use "~/assets/scss/base/media" as *;
+            @use "~/assets/scss/base/units" as *;
+            @use "~/assets/scss/base/variables" as *;
+            @use "~/assets/scss/base/fonts" as *;
+          `
+        },
+      },
+    },
+  },
+});
